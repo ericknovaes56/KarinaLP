3aaa0784441631e7612b87f2272262d4cf27cfe4
erick <ericknovaes56@gmail.com>
2023-11-01 17:31:46 -0300

    'navbar-efect'

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,8 @@
 const btnsNavBar = document.querySelectorAll(".navbtn");
 const line = document.querySelector('.line');
+const menu = document.querySelector(".menu")
 
+const sectionsArray = []
 
 const setLineOnButton = (button) =>{
     var buttonWidth = button.clientWidth;
@@ -12,12 +14,46 @@ const setLineOnButton = (button) =>{
 
 btnsNavBar.forEach((button, i) => {
 
+
+  if (button.parentNode.dataset.to){
+    var getElement = document.querySelector("#"+button.parentNode.dataset.to)
+    if(getElement){
+      sectionsArray.push(getElement)
+    }
+  }
+
   if (i === 0) {
     setLineOnButton(button)
   }
 
-  button.addEventListener("click", () => {
-    setLineOnButton(button)
-  });
 
 });
+
+window.addEventListener("scroll", (e) => {
+  sectionsArray.forEach(section => {
+    var scrollTop = window.scrollY;
+    const posicaoElementoTop = section.offsetTop - menu.clientHeight - 15;
+    const posicaoElementoDown = section.offsetHeight + posicaoElementoTop;
+
+    if (scrollTop < sectionsArray[0].offsetTop - menu.clientHeight - 15){
+      setLineOnButton(btnsNavBar[0])
+      return
+    }
+
+    if (scrollTop >= posicaoElementoTop && scrollTop <= posicaoElementoDown){
+      btnsNavBar.forEach(button => {
+        var parent = button.parentNode
+        if (parent.dataset.to == section.id){
+          setLineOnButton(button)
+        }
+      });
+    }
+
+    
+  });
+});
+
+
+
+
+
